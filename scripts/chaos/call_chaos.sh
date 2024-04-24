906b33c807ea7dc19a718d00d07cf45fadcea75c
Martin Slovík <xslovik@fi.muni.cz>
2024-04-24 21:43:08 +0200

    Adapt for testing

--- scripts/chaos/call_chaos.sh
+++ scripts/chaos/call_chaos.sh
@@ -8,32 +8,30 @@ set -o pipefail
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function usage {
-    echo "usage: $0: <customers|visits|vets> <attacks_enable_exception|attacks_enable_killapplication|attacks_enable_latency|attacks_enable_memory|watcher_enable_component|watcher_enable_controller|watcher_enable_repository|watcher_enable_restcontroller|watcher_enable_service|watcher_disable>"
-    echo "First pick either customers, visits or vets"
+    echo "usage: $0 <port> <customers|visits|vets> <attacks_enable_exception|attacks_enable_killapplication|attacks_enable_latency|attacks_enable_memory|watcher_enable_component|watcher_enable_controller|watcher_enable_repository|watcher_enable_restcontroller|watcher_enable_service|watcher_disable>"
+    echo "First specify the port, then pick either customers, visits, or vets"
     echo "Then pick what to enable. Order matters!"
     echo "Example"
-    echo "./scripts/chaos/call_chaos.sh visits attacks_enable_exception watcher_enable_restcontroller"
+    echo "$0 8082 visits attacks_enable_exception watcher_enable_restcontroller"
     exit 1
 }
 
-if [[ $# -lt 2 ]]; then
+if [[ $# -lt 3 ]]; then
     usage
 fi
 
-export PORT="${PORT:-}"
+PORT="$1"
+shift
 
 while [[ $# > 0 ]]
 do
 key="$1"
 case $1 in
     customers)
-        PORT=8081
         ;;
     visits)
-        PORT=8082
         ;;
     vets)
-        PORT=8083
         ;;
     attacks*)
         ( cd "${ROOT_DIR}" && curl "http://localhost:${PORT}/actuator/chaosmonkey/assaults" -H "Content-Type: application/json" --data @"${1}".json --fail )
@@ -47,3 +45,4 @@ case $1 in
 esac
 shift
 done
+
